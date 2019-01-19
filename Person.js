a5b6b30327bb710781fee5af2eb4192dc491b22a
Jamie W <jamie.won.2000@gmail.com>
2019-01-19 11:54:51 -0500

    Apppearance tings

--- Person.js
+++ Person.js
@@ -20,11 +20,41 @@ class Person {
 		
 		let classesContainer = document.createElement("div");
 		classesContainer.classList.add("swipeClasses");
-		classesContainer.textContent = "Courses: " + courses;
+		classesContainer.textContent = "Courses:";
+		if (courses.length < 2) {
+			classesContainer.textContent +=  courses;	
+		} else {
+			for(let i = 0; i < courses.length;i ++) {
+				let tempContainer = document.createElement("div");
+				if (courses[i+1] !== undefined) {
+					tempContainer.textContent = courses[i] + "   " + courses[i+1];
+				} else {
+					tempContainer.textContent = courses[i];
+				}
+				i ++;
+				classesContainer.appendChild(tempContainer);
+			}
+		}
+		
 		
 		let hobbiesContainer = document.createElement("div");
 		hobbiesContainer.classList.add("swipeClasses");
-		hobbiesContainer.textContent = "Hobbies: " + hobbies;
+		hobbiesContainer.textContent = "Hobbies:";
+
+		if (hobbies.length < 2) {
+			hobbiesContainer.textContent +=  hobbies;	
+		} else {
+			for(let i = 0; i < hobbies.length;i ++) {
+				let tempContainer = document.createElement("div");
+				if (hobbies[i+1] !== undefined) {
+					tempContainer.textContent = hobbies[i] + "   " + hobbies[i+1];
+				} else {
+					tempContainer.textContent = hobbies[i];
+				}
+				i ++;
+				hobbiesContainer.appendChild(tempContainer);
+			}
+		}
 
 		let environmentContainer = document.createElement("div");
 		environmentContainer.classList.add("swipeClasses");
