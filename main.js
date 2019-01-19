7080ca60f354455c992207f891f84b444aa535ae
Jamie W <jamie.won.2000@gmail.com>
2019-01-19 02:01:17 -0500

    Edgar

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-let app = new App();
-
 // Get data from database
 
-let UserData = [
-	["Jane Doe", ["123", "234", "345", "456", "567"], [1,2,3], "quiet cafe"],
-	["Morino Aslan", ["234", "345", "456", "567", "912"], [1,2,3], "library"],
-	["Anna Dragon", ["123", "234", "345", "456", "567"], [1,2,3], "well lit room"],
+var UserData = [
+	["Jane Doe", ["123", "234", "345", "456", "567"], ["Sports"], "quiet cafe"],
+	["Morino Aslan", ["234", "345", "456", "567", "912"], ["Music", "Sports"], "library"],
+	["Anna Dragon", ["123", "234", "345", "456", "567"], ["Music", "Reading", "Iron-Man"], "well lit room"],
 ];
 
 function findPeople() {
 	let users = [];
 	for (let i = 0; i < UserData.length; i ++) {
-		users.push(new Person)
+		users.push(new Person(UserData[i][0], UserData[i][1], UserData[i][2], UserData[i][3]));
 	}
-
+	console.log(users);
+	return users;
 }
+
+let app = new App();
+
