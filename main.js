dd24cfee8f9e9dda7499d5e1ace27e3ed3d396fa
Jamie W <jamie.won.2000@gmail.com>
2019-01-19 01:46:35 -0500

    basic templates

---
+++ main.js
@@ -0,0 +1,17 @@
+let app = new App();
+
+// Get data from database
+
+let UserData = [
+	["Jane Doe", ["123", "234", "345", "456", "567"], [1,2,3], "quiet cafe"],
+	["Morino Aslan", ["234", "345", "456", "567", "912"], [1,2,3], "library"],
+	["Anna Dragon", ["123", "234", "345", "456", "567"], [1,2,3], "well lit room"],
+];
+
+function findPeople() {
+	let users = [];
+	for (let i = 0; i < UserData.length; i ++) {
+		users.push(new Person)
+	}
+
+}
