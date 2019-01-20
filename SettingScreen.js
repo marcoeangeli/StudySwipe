65e451db34973c6e82629485de1d42bb32029721
edgar sarkisian <gesarki@gmail.com>
2019-01-20 08:43:25 -0500

    did settings back end

--- SettingScreen.js
+++ SettingScreen.js
@@ -2,19 +2,33 @@ class SettingScreen {
 	constructor (container) {
 		this.container = container;
 		this.classList = this.container.classList;
+
+		document.querySelector("#updateButton").addEventListener('pointerdown', this.buttonClick);
 		
 	}
 
-	buttonClick(event) {
-		// FInds out what the change is to be done
-		let howChange = document.querySelector("select").value;
+	async buttonClick() {
+		console.log("came here");
+		var email = document.getElementById('email').value;
+	      var password = document.getElementById('setting-password').value;
 
-		// Finds out waht needs to be changed
-		let fieldChange = document.querySelectorAll("select")[1].value;
-		
-		let propertyName = document.querySelector("#changeVal").value;
+	      var course1 = document.getElementById('setting-course1').value;
+	      var course2 = document.getElementById('setting-course2').value;
+	      var course3 = document.getElementById('setting-course3').value;
+	      var course4 = document.getElementById('setting-course4').value;
+	      var course5 = document.getElementById('setting-course5').value;
 
-		console.log(howChange + " " + fieldChange + " " + propertyName);
+	      var hobby1 = document.getElementById('setting-hobby1').value;
+	      var hobby2 = document.getElementById('setting-hobby2').value;
+	      var hobby3 = document.getElementById('setting-hobby3').value;
+
+	      var enviroment = document.getElementById('setting-enviroment').value;
+
+	      let userId = app.userId;
+	      firebase.database().ref('users/' + userId).set({name: "Edgar Drakula", courses: [course1, course2, course3, course4, course5], hobbies: [hobby1, hobby2, hobby3], enviroment: enviroment});
+	      document.getElementById('setting-feedback').value = "success";
 	}
+
+
 
 }
